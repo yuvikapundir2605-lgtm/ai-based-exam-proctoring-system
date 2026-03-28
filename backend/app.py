7e23ed5f919465e2237cc88f512a88624f9a95c1
yuvikapundir2605-lgtm <yuvikapundir2605@gmail.com>
2026-03-28 12:58:22 +0530

    Basic Behaviour Tracking

--- backend/app.py
+++ backend/app.py
@@ -4,37 +4,38 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-# Start camera
+
 camera = cv2.VideoCapture(0)
 
-# Load face detection model
+
 face_cascade = cv2.CascadeClassifier(
     cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
 )
 
-# ✅ Global variable (important)
+status_change_count = 0
 last_status = ""
 
-# Logging function
+
 def log_event(message):
     with open("log.txt", "a") as f:
         f.write(f"{datetime.now()} - {message}\n")
 
 def generate_frames():
-    global last_status   # ✅ use global
+    global last_status, status_change_count
+     
 
     while True:
         success, frame = camera.read()
         if not success:
             break
 
-        # Convert to grayscale
+        
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        # Detect faces
+        
         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-        # 🧠 Proctoring logic
+        
         if len(faces) == 0:
             status = "No Face Detected"
 
@@ -44,25 +45,26 @@ def generate_frames():
         else:
             status = "OK"
 
-        # ✅ Smart logging (only when status changes)
+        
         if status != last_status:
             log_event(status)
+            status_change_count += 1
             last_status = status
 
-        # Draw rectangles
+       
         for (x, y, w, h) in faces:
             cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-        # Show status on screen
+        
         cv2.putText(frame, status, (20, 50),
                     cv2.FONT_HERSHEY_SIMPLEX, 1,
                     (0, 0, 255), 2)
 
-        # Convert frame for browser
+        
         ret, buffer = cv2.imencode('.jpg', frame)
         frame = buffer.tobytes()
 
-        # Stream to browser
+        
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
@@ -70,5 +72,18 @@ def generate_frames():
 def video():
     return Response(generate_frames(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/suspicious')
+def suspicious():
+    if status_change_count > 10:
+        return "⚠ Suspicious Activity Detected"
+    else:
+        return "✅ Normal Behavior"
+
+
+@app.route('/status')
+def get_status():
+    global last_status
+    return last_status
 
 app.run(debug=True)
